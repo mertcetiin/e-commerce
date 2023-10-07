5be5debe4b692e70bb9bf55d8eaa575063d05a06
mertcetiin <mrtctn506@gmail.com>
2023-10-07 18:20:28 +0300

    shop.tsx products listed

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,13 +1,13 @@
 import Shirt from '../assets/images/shirt.avif';
 
-type Product = {
+type ProductHome = {
     id: number;
     img: string;
     price: string;
     info: string;
 }
 
-export let HomeProducts: Product[] = ([
+export let HomeProducts: ProductHome[] = ([
     {
         id: 1,
         img: Shirt,
@@ -43,5 +43,101 @@ export let HomeProducts: Product[] = ([
         img: Shirt,
         price: '$10',
         info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+])
+
+
+type ProductShop = {
+    id: number;
+    img: string;
+    price: string;
+    name: string;
+    info: string;
+}
+
+export let ShopProduct: ProductShop[] = ([
+    {
+        id: 1,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 2,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 3,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 4,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 5,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 6,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 7,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 8,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 9,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 10,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 11,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
+    },
+    {
+        id: 12,
+        img: Shirt,
+        price: '$15',
+        name: 'Shirt',
+        info: 'Clothes',
     },
 ])
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,25 +1,33 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import { useState } from 'react';
+import { ShopProduct } from "./Products";
 
 function Shop() {
+
+    const [shopProducts, setShopProducts] = useState(ShopProduct);
+
     return (
         <div>
             <Header />
-
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap -m-4">
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                                <p className="mt-1">$16.00</p>
+                        {shopProducts.map((item) => (
+                            <div key={item.id} className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a className="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={item.img} />
+                                </a>
+                                <div className="mt-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{item.info}</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">{item.name}</h2>
+                                    <p className="mt-1">{item.price}.00</p>
+
+                                </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
+
                 </div>
             </section>
 
