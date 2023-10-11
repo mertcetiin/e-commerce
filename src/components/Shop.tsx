c737dc55602d94faa917244b6ede2445db0c05f8
mertcetiin <mrtctn506@gmail.com>
2023-10-11 23:36:24 +0300

    basic design made

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -22,11 +22,11 @@ function Shop({ isLikeCounter }: { isLikeCounter: number }) {
                                 <div className="relative group">
                                     <a className="block relative h-48 rounded overflow-hidden">
                                         <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={item.img} />
-                                        <div className="overlay absolute bottom-0 left-0 w-full h-12 bg-white opacity-0 group-hover:opacity-80 cursor-pointer transition-opacity duration-300 flex items-center justify-center">
-                                            <button className="-ml-20">
+                                        <div className="overlay absolute bottom-0 left-0 w-full h-12 bg-white opacity-0 group-hover:opacity-80 transition-opacity duration-300 flex items-center justify-center">
+                                            <button className="ml-0 cursor-pointer border p-2.5 rounded-full">
                                                 <GrFavorite />
                                             </button>
-                                            <button>
+                                            <button className="ml-20 cursor-pointer border p-2.5 rounded-full">
                                                 <FaShoppingBag />
                                             </button>
                                         </div>
