ed2e867344d805097528fc245eb062190ed1a14d
mertcetiin <mrtctn506@gmail.com>
2023-10-18 16:59:24 +0300

    favorite.tsx clearFavorite added

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -2,6 +2,7 @@ import { useStore } from '../states/store';
 
 function Favorite() {
     const homeState = useStore((state) => state.favorite);
+    const clearFavorite = useStore((state) => state.clearFavorite);
 
     return (
         <section className="text-gray-600 body-font overflow-hidden">
@@ -51,7 +52,7 @@ function Favorite() {
                                 <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
                                 <div className="flex">
                                     <span className="title-font font-medium text-2xl text-gray-900">{item.price}.00</span>
-                                    <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                                    <button onClick={() => clearFavorite(item.id)} className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                                             <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                                         </svg>
