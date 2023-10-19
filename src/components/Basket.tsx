05c1ebb61d7b868da91eea75da1d4bb09f69934d
mertcetiin <mrtctn506@gmail.com>
2023-10-19 14:01:53 +0300

    basket.tsx shopProducts listed

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,8 +1,19 @@
+import { useStore } from "../states/store";
 
 
 function Basket() {
+
+    const shopState = useStore((state) => state.basket)
+
     return (
-        <div>Sepet</div>
+        <div>
+            {shopState.map((item) => (
+                <div key={item.id}>
+                    <h2>{item.name}</h2>
+                    <h3>{item.price}</h3>
+                </div>
+            ))}
+        </div>
     )
 }
 
